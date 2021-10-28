75687fcdcde4bc47f0420c58b57b931a937af7ba
Luiza Valezim Augusto Pinto <62949391+LuizaValezim@users.noreply.github.com>
2021-10-28 14:26:53 -0300

    Adicionando dict e etc

--- encode_versaoAlunos.py
+++ encode_versaoAlunos.py
@@ -1,5 +1,5 @@
-import matplotlib
 import numpy as np
+import sounddevice as sd
 
 dict_frequencies = {1: [697, 1209], 
                     2: [697, 1336], 
@@ -18,53 +18,49 @@ dict_frequencies = {1: [697, 1209],
                     'X': [941, 1209],
                     '#': [941, 1477]}
 
-def getFrequencies(n):
-
 
 def signal_handler(signal, frame):
         print('You pressed Ctrl+C!')
         sys.exit(0)
+
 
 #converte intensidade em Db, caso queiram ...
 def todB(s):
     sdB = 10*np.log10(s)
     return(sdB)
 
+
 def main():
     print("Inicializando encoder")
     
     numero = input("Escolha um número de 1 à 9 ou A,B,C,D ou X,#: ")
-     #declare um objeto da classe da sua biblioteca de apoio (cedida)    
-    #declare uma variavel com a frequencia de amostragem, sendo 44100
-    
-    #voce importou a bilioteca sounddevice como, por exemplo, sd. entao
-    # os seguintes parametros devem ser setados:
-    
-    
-    duration = 2    #tempo em segundos que ira emitir o sinal acustico 
-      
-#relativo ao volume. Um ganho alto pode saturar sua placa... comece com .3    
+
+    #relativo ao volume. Um ganho alto pode saturar sua placa... comece com .3    
     gainX  = 0.3
     gainY  = 0.3
-
+    #declare uma variavel com a frequencia de amostragem, sendo 44100
+    fs = 44100 # Hz
+    duration = 2    #tempo em segundos que ira emitir o sinal acustico 
 
     print("Gerando Tons base")
-    
-    #gere duas senoides para cada frequencia da tabela DTMF ! Canal x e canal y 
-    #use para isso sua biblioteca (cedida)
+
+    sen1 = dict_frequencies[numero][0]
+    sen2 = dict_frequencies[numero][1]
+    array = sen1 + sen2
+
+    recording = sd.playrec(array, fs, channels=2) 
+
     #obtenha o vetor tempo tb.
     #deixe tudo como array
 
-    #printe a mensagem para o usuario teclar um numero de 0 a 9. 
-    #nao aceite outro valor de entrada.
-    print("Gerando Tom referente ao símbolo : {}".format(NUM))
+    print("Gerando Tom referente ao símbolo : {}".format(numero))
     
     
     #construa o sunal a ser reproduzido. nao se esqueca de que é a soma das senoides
     
     #printe o grafico no tempo do sinal a ser reproduzido
     # reproduz o som
-    sd.play(tone, fs)
+    sd.play(recording)
     # Exibe gráficos
     plt.show()
     # aguarda fim do audio

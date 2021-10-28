5b3b56f14ad193e9d354faf156c1315dbc553339
Luiza Valezim Augusto Pinto <62949391+LuizaValezim@users.noreply.github.com>
2021-10-28 14:54:07 -0300

    Encode feito com sucesso

--- encode_versaoAlunos.py
+++ encode_versaoAlunos.py
@@ -1,16 +1,20 @@
 import numpy as np
 import sounddevice as sd
+from suaBibSignal import *
+import time
 
-dict_frequencies = {1: [697, 1209], 
-                    2: [697, 1336], 
-                    3: [697, 1477], 
-                    4: [770, 1209], 
-                    5: [770, 1336],
-                    6: [770, 1477],
-                    7: [852, 1209], 
-                    8: [852, 1336],
-                    9: [852, 1447],
-                    0: [941, 1336],
+bib = signalMeu()
+
+dict_frequencies = {'1': [697, 1209], 
+                    '2': [697, 1336], 
+                    '3': [697, 1477], 
+                    '4': [770, 1209], 
+                    '5': [770, 1336],
+                    '6': [770, 1477],
+                    '7': [852, 1209], 
+                    '8': [852, 1336],
+                    '9': [852, 1447],
+                    '0': [941, 1336],
                     'A': [697, 1633],
                     'B': [770, 1633],
                     'C': [852, 1633],
@@ -40,27 +44,24 @@ def main():
     gainY  = 0.3
     #declare uma variavel com a frequencia de amostragem, sendo 44100
     fs = 44100 # Hz
-    duration = 2    #tempo em segundos que ira emitir o sinal acustico 
+    duration = 3    #tempo em segundos que ira emitir o sinal acustico 
 
     print("Gerando Tons base")
 
-    sen1 = dict_frequencies[numero][0]
-    sen2 = dict_frequencies[numero][1]
-    array = sen1 + sen2
+    f1 = dict_frequencies[numero][0]
+    sen1 = bib.generateSin(f1, gainX, duration, fs)
+    f2 = dict_frequencies[numero][1]
+    sen2 = bib.generateSin(f2, gainY, duration, fs)
 
-    recording = sd.playrec(array, fs, channels=2) 
-
-    #obtenha o vetor tempo tb.
-    #deixe tudo como array
+    array = sen1[1] + sen2[1]
 
     print("Gerando Tom referente ao símbolo : {}".format(numero))
     
-    
-    #construa o sunal a ser reproduzido. nao se esqueca de que é a soma das senoides
-    
     #printe o grafico no tempo do sinal a ser reproduzido
+    bib.plotFFT(array, fs)
     # reproduz o som
-    sd.play(recording)
+    sd.play(array)
+    sd.wait()
     # Exibe gráficos
     plt.show()
     # aguarda fim do audio

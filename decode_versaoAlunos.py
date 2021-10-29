52a5251892cca92b3e922b1856476fca81a36a7a
Luiza Valezim Augusto Pinto <62949391+LuizaValezim@users.noreply.github.com>
2021-10-29 17:46:52 -0300

    oi

--- decode_versaoAlunos.py
+++ decode_versaoAlunos.py
@@ -2,6 +2,9 @@ import numpy as np
 import sounddevice as sd
 from suaBibSignal import *
 import time
+import peakutils
+from peakutils.plot import plot as pplot
+from matplotlib import pyplot
 
 #Importe todas as bibliotecas
 
@@ -22,8 +25,7 @@ def main():
     # os seguintes parametros devem ser setados:
     
     sd.default.samplerate = fs #taxa de amostragem
-    sd.default.channels = 2  #voce pode ter que alterar isso dependendo da sua placa
-
+    sd.default.channels = 1  #voce pode ter que alterar isso dependendo da sua placa
 
     # faca um printo na tela dizendo que a captacao comecará em n segundos. e entao
     tempoEspera = 3
@@ -39,7 +41,7 @@ def main():
     numAmostras = fs*duration
     freqDeAmostragem = fs
 
-    audio = sd.rec(int(numAmostras), freqDeAmostragem, channels=1)
+    audio = sd.rec(int(numAmostras), freqDeAmostragem, channels=1 )
     sd.wait()
     print("...     FIM")
     
@@ -47,18 +49,24 @@ def main():
     #analise sua variavel "audio". pode ser um vetor com 1 ou 2 colunas, lista ...
     #grave uma variavel com apenas a parte que interessa (dados)
     
+    print(audio[0])
 
     # use a funcao linspace e crie o vetor tempo. Um instante correspondente a cada amostra!
+    inicio = 0
+    fim = 10
+    numPontos = 5
     t = np.linspace(inicio, fim, numPontos)
 
-    # plot do gravico  áudio vs tempo!
+    # plot do grafico  áudio vs tempo!
    
     
     ## Calcula e exibe o Fourier do sinal audio. como saida tem-se a amplitude e as frequencias
-    xf, yf = signal.calcFFT(y, fs)
+    xf, yf = bib.calcFFT(audio[:,0], fs)
     plt.figure("F(y)")
     plt.plot(xf,yf)
     plt.grid()
+    plt.xlabel("Frequência")
+    plt.ylabel("Intensidade")
     plt.title('Fourier audio')
     
 
@@ -67,12 +75,20 @@ def main():
     #voce deve tambem evitar que dois picos proximos sejam identificados, pois pequenas variacoes na
     #frequencia do sinal podem gerar mais de um pico, e na verdade tempos apenas 1.
    
-    index = peakutils.indexes(,,)
+    index = peakutils.indexes(yf,thres=0.5, min_dist=30)
     
     #printe os picos encontrados! 
+    print(index)
+    print(t[index], yf[index])
+    pyplot.figure(figsize=(10,6))
+    pplot(t, yf, index)
+    plt.xlabel("Tempo")
+    plt.ylabel("Áudio")
+    pyplot.title('First estimate')
     
     #encontre na tabela duas frequencias proximas às frequencias de pico encontradas e descubra qual foi a tecla
-    #print a tecla.
+    
+    # print(f"Com a frequência {} e {}, a tecla pressionada foi {}")
     
   
     ## Exibe gráficos
